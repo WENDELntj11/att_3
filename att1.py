a979b2a7ad72f988fb600ac095e5c6de17e266c8
WENDELntj11 <141692988+WENDELntj11@users.noreply.github.com>
2023-09-19 18:58:58 -0300

    Update att1.py

--- att1.py
+++ att1.py
@@ -1,33 +1,33 @@
 def calcular_signo(dia, mes):
     if (mes == 3 and dia >= 21) or (mes == 4 and dia <= 19):
-        return "Áries"
+        return 'Áries'
     elif (mes == 4 and dia >= 20) or (mes == 5 and dia <= 20):
-        return "Touro"
+        return 'Touro'
     elif (mes == 5 and dia >= 21) or (mes == 6 and dia <= 20):
-        return "Gêmeos"
+        return 'Gêmeos'
     elif (mes == 6 and dia >= 21) or (mes == 7 and dia <= 22):
-        return "Câncer"
+        return 'Câncer'
     elif (mes == 7 and dia >= 23) or (mes == 8 and dia <= 22):
-        return "Leão"
+        return 'Leão'
     elif (mes == 8 and dia >= 23) or (mes == 9 and dia <= 22):
-        return "Virgem"
+        return 'Virgem'
     elif (mes == 9 and dia >= 23) or (mes == 10 and dia <= 22):
-        return "Libra"
+        return 'Libra'
     elif (mes == 10 and dia >= 23) or (mes == 11 and dia <=21):
-        return "Escorpião"
+        return 'Escorpião'
     elif (mes == 11 and dia >= 22) or (mes == 12 and dia <= 21):
-        return "Sagitário"
+        return 'Sagitário'
     elif (mes == 12 and dia >= 22) or (mes == 1 and dia <= 19):
-        return "Capricórnio"
+        return 'Capricórnio'
     elif (mes == 1 and dia >= 20) or (mes == 2 and dia <= 18):
-        return "Aquário"
+        return 'Aquário'
     else:
-        return "Peixes" 
+        return 'Peixes' 
 
 # Solicitar o ano de nascimento do usuário
-ano_nascimento = int(input("Digite seu ano de nascimento: "))
-mes_nascimento = int(input("Digite seu mês de nascimento: "))
-dia_nascimento = int(input("Digite seu dia de nascimento: "))
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+mes_nascimento = int(input("Digite o mês de nascimento: "))
+dia_nascimento = int(input("Digite o dia de nascimento: "))
 
 # Obter a data atual
 from datetime import datetime
