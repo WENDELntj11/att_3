8dd11c4d5f7fa165c0b3d4c8925135829a95ab10
WENDELntj11 <141692988+WENDELntj11@users.noreply.github.com>
2023-09-19 19:06:04 -0300

    Update att17.py

--- att17.py
+++ att17.py
@@ -1,6 +1,6 @@
 def calculadora_de_impostos():
-    valor_compra = float(input("Insira o valor da compra: "))
-    valor_imposto = float(input("Insira o valor do imposto a ser adicionado: "))
+    valor_compra = float(input('Insira um valor da compra: '))
+    valor_imposto = float(input("Insira um valor do imposto a ser adicionado: "))
     valor_total = valor_compra + valor_imposto
     print("Valor total da compra com imposto:", round(valor_total, 2))
 
