0121d99fb7cd93e842604949b7cba18e235dc080
WENDELntj11 <141692988+WENDELntj11@users.noreply.github.com>
2023-09-19 19:08:41 -0300

    Update att5.py

--- att5.py
+++ att5.py
@@ -1,8 +1,8 @@
 def exibir_tarefas(tarefas):
-    print("Lista das Tarefas:")
+    print('Lista das Tarefas:')
     for i, tarefa in enumerate(tarefas, start=1):
-        status = "Concluída" if tarefa["concluida"] else "Pendente"
-        print(f"{i}. [{status}] {tarefa['descricao']}")
+        status = "Concluída" if tarefa["concluida"] else 'Pendente'
+        print(f"{i}. [{status}] {tarefa["descricao"]}")
 def remover_tarefa(tarefas, indice):
     if 0 <= indice < len(tarefas):
         del tarefas[indice]
@@ -18,13 +18,13 @@ def salvar_tarefas_arquivo(tarefas, nome_arquivo):
 # Função para carregar a lista de tarefas de um arquivo
 def carregar_tarefas_arquivo(nome_arquivo):
     tarefas = []
-    if os.path.exists(nome_arquivo):
-        with open(nome_arquivo, "r") as arquivo:
-            for linha in arquivo:
+ if os.path.exists(nome_arquivo):
+  with open(nome_arquivo, 'r') as arquivo:
+     for linha in arquivo:
                 descricao, concluida_str = linha.strip().split(",")
                 concluida = concluida_str == "True"
                 tarefas.append({"descricao": descricao, "concluida": concluida})
-    return tarefas
+return tarefas
 
 # Nome do arquivo para armazenar as tarefas
 nome_arquivo = "tarefas.txt"
@@ -44,18 +44,18 @@ while True:
 
     if escolha == "1":
         exibir_tarefas(tarefas)
-    elif escolha == "2":
+     elif escolha == "2":
         descricao = input("Digite a descrição da tarefa: ")
         adicionar_tarefa(tarefas, descricao)
         salvar_tarefas_arquivo(tarefas, nome_arquivo)
         print("Tarefa adicionada com sucesso.")
-    elif escolha == "3":
+     elif escolha == "3":
         exibir_tarefas(tarefas)
         indice = int(input("Digite o número da tarefa que deseja marcar como concluída: ")) - 1
         marcar_tarefa_concluida(tarefas, indice)
         salvar_tarefas_arquivo(tarefas, nome_arquivo)
         print("Tarefa marcada como concluída.")
-    elif escolha == "4":
+     elif escolha == "4":
         exibir_tarefas(tarefas)
         indice = int(input("Digite o número da tarefa que deseja remover: ")) - 1
         remover_tarefa(tarefas, indice)
